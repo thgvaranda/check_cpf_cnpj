b5054ac4cbac9e2cd9f59ac56017437a18ffcff6
Thiago Varanda <thiago.varanda@totalip.com.br>
2016-08-09 17:50:55 -0300

    Add namespaces on classes Cpf and Cnpj.
    Signed-off-by: Thiago Varanda <thiago.varanda@totalip.com.br>

--- lib/cnpj/cnpj.rb
+++ lib/cnpj/cnpj.rb
@@ -1,32 +1,34 @@
-class Cnpj
+module CheckCpfCnpj
+  class Cnpj
 
-  def initialize(number=nil)
-    return false if number.nil?
-    @number = number.to_s
-  end
+    def initialize(number=nil)
+      return false if number.nil?
+      @number = number.to_s
+    end
 
-  def only_numbers
-    return @number.gsub(".", "").gsub("-", "").gsub("/", "")
-  end
+    def only_numbers
+      return @number.gsub(".", "").gsub("-", "").gsub("/", "")
+    end
 
-  def formatted
-    return @number.insert(2, '.').insert(6, '.').insert(10, '/').insert(15, '-')
-  end
+    def formatted
+      return @number.insert(2, '.').insert(6, '.').insert(10, '/').insert(15, '-')
+    end
 
-  def valid?
-    return false if @number == '00000000000000'
-    return false if @number[12].to_i != check_digits(@number[0..11].split("").reverse)
-    return false if @number[13].to_i != check_digits(@number[0..12].split("").reverse)
-    return true
-  end
+    def valid?
+      return false if @number == '00000000000000'
+      return false if @number[12].to_i != check_digits(@number[0..11].split("").reverse)
+      return false if @number[13].to_i != check_digits(@number[0..12].split("").reverse)
+      return true
+    end
 
-  def check_digits(digits)
-    #receives digits reverted to simplify iteration
-    #array_check also is reverted to iteration
-    array_check = [2,3,4,5,6,7,8,9,2,3,4,5,6]
-    sum = 0
-    digits.each_with_index{ |digit, index| sum += digit.to_i * array_check[index].to_i }
-    mod = sum % 11
-    if mod < 2 then 0 else 11-mod end
+    def check_digits(digits)
+      #receives digits reverted to simplify iteration
+      #array_check also is reverted to iteration
+      array_check = [2,3,4,5,6,7,8,9,2,3,4,5,6]
+      sum = 0
+      digits.each_with_index{ |digit, index| sum += digit.to_i * array_check[index].to_i }
+      mod = sum % 11
+      if mod < 2 then 0 else 11-mod end
+    end
   end
 end
--- lib/cpf/cpf.rb
+++ lib/cpf/cpf.rb
@@ -1,40 +1,42 @@
-class Cpf
+module CheckCpfCnpj
+  class Cpf
 
-  INVALID_CPF = [
-    '11111111111', '22222222222', '33333333333',
-    '44444444444', '55555555555', '66666666666',
-    '77777777777', '88888888888', '99999999999',
-    '00000000000'
-  ]
+    INVALID_CPF = [
+      '11111111111', '22222222222', '33333333333',
+      '44444444444', '55555555555', '66666666666',
+      '77777777777', '88888888888', '99999999999',
+      '00000000000'
+    ]
 
-  def initialize(number)
-    return false if number.nil?
-    @number = number.to_s
-  end
-
-  def only_numbers
-    return @number.gsub(".", "").gsub("-", "")
-  end
-
-  def formatted
-    return @number.insert(3, '.').insert(7, '.').insert(11, '-')
-  end
-
-  def valid?
-    return false if INVALID_CPF.include?(@number)
-    return false if @number[9].to_i != check_digits(@number[0..8].split(""))
-    return false if @number[10].to_i != check_digits(@number[0..9].split(""))
-    return true
-  end
-
-  def check_digits(digits)
-    prod = digits.size + 1
-    sum = 0
-    digits.each do |digit|
-      sum += digit.to_i * prod
-      prod -= 1
+    def initialize(number)
+      return false if number.nil?
+      @number = number.to_s
     end
-    mod = sum%11
-    if mod < 2 then 0 else 11-mod end
+
+    def only_numbers
+      return @number.gsub(".", "").gsub("-", "")
+    end
+
+    def formatted
+      return @number.insert(3, '.').insert(7, '.').insert(11, '-')
+    end
+
+    def valid?
+      return false if INVALID_CPF.include?(@number)
+      return false if @number[9].to_i != check_digits(@number[0..8].split(""))
+      return false if @number[10].to_i != check_digits(@number[0..9].split(""))
+      return true
+    end
+
+    def check_digits(digits)
+      prod = digits.size + 1
+      sum = 0
+      digits.each do |digit|
+        sum += digit.to_i * prod
+        prod -= 1
+      end
+      mod = sum%11
+      if mod < 2 then 0 else 11-mod end
+    end
   end
 end
